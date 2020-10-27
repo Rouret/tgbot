840c6d8ce60273066b306b235bd73ee041ad4036
Rouret Lucas <lucas.rouretperso@gmail.com>
2020-10-27 22:27:01 +0100

    add join

---
+++ commands/join.js
@@ -0,0 +1,17 @@
+const ytdl = require('ytdl-core');
+const fs = require('fs');
+module.exports = {
+    name: 'join',
+    aliases: ['j'],
+    description: 'oups',
+    async execute(client, api, config, message, args) {
+        const connection = await message.member.voice.channel.join();
+        if (message.member.voice.channel) {
+            const dispatcher = connection.play(fs.createReadStream('./sounds/alak.mp3'));
+            dispatcher.setVolume(1);
+            dispatcher.on('finish', () => {
+                connection.disconnect();
+            });
+        }
+    },
+};
