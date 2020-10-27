6f41a6ffa9eb5026e569ba979d15cd46cf5091a8
Rouret Lucas <lucas.rouretperso@gmail.com>
2020-10-27 20:48:42 +0100

    add faim commande

---
+++ commands/faim.js
@@ -0,0 +1,16 @@
+const fetch = require("node-fetch");
+module.exports = {
+    name: 'faim',
+    aliases: ['food', 'toutdanslegosier'],
+    description: 'Pour te faire regretter tes pâtes',
+    execute(client, api, config, message, args) {
+        message.delete()
+        fetch("https://foodish-api.herokuapp.com/api", {})
+            .then((response) => response.json())
+            .then((data) => {
+                message.channel.send(data.image);
+            });
+    },
+};
+
+// 509329395949764609
