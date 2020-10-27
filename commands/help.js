fa5b5c3c134f39c77e3dbf0b9cb4a978ef691464
Rouret Lucas <lucas.rouretperso@gmail.com>
2020-10-27 20:40:32 +0100

    update help

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,9 @@
 const { prefix } = require('../config.json');
-
+const Discord = require('discord.js');
 module.exports = {
     name: 'help',
     description: 'List all of my commands or info about a specific command',
-    aliases: ['command'],
+    aliases: ['h'],
     usage: '[command name]',
     cooldown: 5,
     execute(client, api, config, message, args) {
@@ -15,32 +15,39 @@ module.exports = {
             data.push(commands.map(command => command.name).join(', '));
             data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-            return message.author.send(data, { split: true })
-                .then(() => {
-                    if (message.channel.type == 'dm') return;
-                    message.reply('I\'ve sent you a DM with all my commands!');
-                })
-                .catch(error => {
-                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                    message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+            const exampleEmbed = new Discord.MessageEmbed()
+                .setColor("orange")
+                .setTitle("Besoin d'aide la ziz ?")
+                .setURL("https://pornhub.com/")
+                .setAuthor(
+                    "Tg bot",
+                    "https://sayingimages.com/wp-content/uploads/i-needs-help-help-meme.jpg",
+                    ""
+                )
+                .setThumbnail(
+                    "https://sayingimages.com/wp-content/uploads/i-needs-help-help-meme.jpg"
+                )
+                .addFields({
+                    name: "Voila pour toi bb",
+                    value: commands.map(command => command.name).join('\n'),
                 });
+
+            return message.channel.send(exampleEmbed)
         }
 
         const name = args[0].toLowerCase();
-		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
-		if (!command) {
-			return message.reply('that\'s not a valid command!');
-		}
+        if (!command) {
+            return message.reply('Quoi ?');
+        }
 
-		data.push(`**Name:** ${command.name}`);
+        data.push(`**Commande:** !${command.name}`);
 
-		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-		if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Usage:** ${prefix}${command.usage}`);
+        if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+        if (command.description) data.push(`**Description:** ${command.description}`);
+        if (command.usage) data.push(`**Usage:** ${prefix}${command.usage}`);
 
-		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-
-		message.channel.send(data, { split: true });
+        message.channel.send(data, { split: true });
     },
 };
