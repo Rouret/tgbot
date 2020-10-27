9f664e7f76542dcd9d437d89578333b144922444
Rouret Lucas <lucas.rouretperso@gmail.com>
2020-10-27 22:28:38 +0100

    forget

--- commands/join.js
+++ commands/join.js
@@ -5,6 +5,7 @@ module.exports = {
     aliases: ['j'],
     description: 'oups',
     async execute(client, api, config, message, args) {
+        message.delete()
         const connection = await message.member.voice.channel.join();
         if (message.member.voice.channel) {
             const dispatcher = connection.play(fs.createReadStream('./sounds/alak.mp3'));
