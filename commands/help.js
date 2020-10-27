04d6ebe8d5d16b6f8cc76cab1a7d9fdb0fc4c5ea
Rouret Lucas <lucas.rouretperso@gmail.com>
2020-10-27 21:26:18 +0100

    change message

--- commands/help.js
+++ commands/help.js
@@ -11,13 +11,9 @@ module.exports = {
         const { commands } = message.client;
 
         if (!args.length) {
-            data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => command.name).join(', '));
-            data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
-
             const exampleEmbed = new Discord.MessageEmbed()
                 .setColor("orange")
-                .setTitle("Besoin d'aide la ziz ?")
+                .setTitle("Besoin d'aide la ziz ? fait `!help COMMANDE`")
                 .setURL("https://pornhub.com/")
                 .setAuthor(
                     "Tg bot",
