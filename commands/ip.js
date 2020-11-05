a7b49e1364efe3df0ae21207e5a7ed847752114e
Rouret Lucas <lucas.rouretperso@gmail.com>
2020-11-05 17:41:56 +0100

    add ip info

--- commands/ip.js
+++ commands/ip.js
@@ -24,7 +24,6 @@ module.exports = {
             pgClient
                 .query(total)
                 .then(res => {
-                    console.log(response)
                     response.description = "Nombre de noms de domaines découvert: "
                     response.result = res.rows[0].total
                     const exampleEmbed = new Discord.MessageEmbed()
@@ -39,7 +38,68 @@ module.exports = {
                         });
                     return message.channel.send(exampleEmbed);
                 })
-                .catch(e => console.error(e.stack))
+        } else if (args.length === 1 && args[0] === "info") {
+            var resul1, result2;
+            pgClient
+                .query(total)
+                .then(res => resul1 = res.rows[0].total)
+            setTimeout(() => {
+                pgClient
+                    .query(total)
+                    .then(res => {
+                        const TOTAL_IP = 4294967296
+                        response.description = "Actuellement un vitesse de:"
+                            // + " @ip/s"
+                        current = parseInt(res.rows[0].total)
+                        seconde = current - resul1
+                        minute = seconde * 60
+                        hours = minute * 60
+                        day = hours * 24
+
+                        response.speed =
+                            "*@IP/s:*  `" + seconde + "`\n" +
+                            "*@IP/m:* `" + minute + "`\n" +
+                            "*@IP/h:* `" + hours + "`\n" +
+                            "*@IP/d:* `" + day + "`"
+
+                        predict_tomo = current + day
+                        predict_week = current + day * 7
+
+                        response.predict =
+                            "*Demain:*`" + predict_tomo + "`\n" +
+                            "*Semaine prochaine*`" + predict_week + "`"
+
+                        response.total = ((current / TOTAL_IP) * 100).toFixed(5) + " %"
+                        message.channel.send({
+                            embed: {
+                                color: 3447003,
+                                author: {
+                                    name: client.user.username,
+                                    icon_url: client.user.avatarURL()
+                                },
+                                title: "Information",
+                                fields: [{
+                                        name: "Vitesse",
+                                        value: response.speed
+                                    },
+                                    {
+                                        name: "Prévision",
+                                        value: response.predict
+                                    },
+                                    {
+                                        name: "% des @ip disponibles sur internet",
+                                        value: response.total
+                                    }
+                                ],
+                                timestamp: new Date(),
+                                footer: {
+                                    icon_url: client.user.avatarURL(),
+                                    text: "TG bot"
+                                }
+                            }
+                        });
+                    })
+            }, 1000)
         } else if (args.length === 1) {
             const search = `SELECT name FROM data WHERE name LIKE '%${args[0]}%' LIMIT 10`;
             pgClient.query(search, (err, res) => {
